b2eb5abd57ecad0b6b5331afd004e3a12b1efda4
sentientmachin3 <davideb1912@gmail.com>
2023-05-20 11:28:15 +0200

    fix: not running on CTRL-C press

--- src/fzf.rs
+++ src/fzf.rs
@@ -11,6 +11,9 @@ pub fn fzf(worktrees: &Vec<String>) -> String {
     let item_reader = SkimItemReader::default();
     let items = item_reader.of_bufread(Cursor::new(input));
     let output = Skim::run_with(&options, Some(items)).unwrap();
+    if output.is_abort {
+        std::process::exit(0);
+    }
     let selected_item = output.selected_items.get(0).unwrap().output().to_string();
     return selected_item;
 }
